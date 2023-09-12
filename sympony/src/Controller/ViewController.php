51f43218229b96dbfe2e091232eb40a6972ffbb5
Akash Boora <booraakash@gmail.com>
2023-09-12 19:16:28 +0530

    created twig file

---
+++ sympony/src/Controller/ViewController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ViewController extends AbstractController
+{
+    /**
+     * @Route("/view", name="app_view")
+     */
+    public function index(): Response
+    {
+        $tag = date("l");
+        $user  = [
+            "name" => "udemy",
+            "nachname" => "dev",
+            "alter" =>  99
+        ];
+        $a = ["Hey","Hai","Oy"];
+        return $this->render('view/index.html.twig', [
+            'd' => $tag,
+            'user' => $user,
+            'a' => $a
+        ]);
+    }
+}
