306d78b34618d918c5689081d97de54c834aeddb
Akash Boora <booraakash@gmail.com>
2023-09-12 18:12:53 +0530

    created new controller and added new route

---
+++ sympony/src/Controller/MainController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/main", name="app_main")
+     */
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/start/{name?}", name="start")
+     */
+    public function start(Request $request, $name){
+        $nr = random_int(1,100);
+        return new Response("<h1>Hello $name $nr</h1>");
+    } 
+}
